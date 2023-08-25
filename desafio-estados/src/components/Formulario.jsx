24cadf61e45ba856bf4dbdeb6488dd8d3dc6318a
Jotape <juanpa1999a@gmail.com>
2023-08-25 12:14:57 -0400

    Se levanta estructura base de formulario y primer input

--- desafio-estados/src/components/Formulario.jsx
+++ desafio-estados/src/components/Formulario.jsx
@@ -1,1 +1,18 @@
+const Formulario = () => {
+    //Estados del formulario
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [email, setEmail] = useState('');
+    const [verif, setVerif] = useState('');
+
+    return (
+        <>
+            <form onSubmit={} className="formulario">
+                <div className="form-group">
+                    <input type="text" name="nombre" placeholder="Nombre" className="formControl" onChange={(e) => setNombre(e.target.value)} value={nombre}/>
+                </div>
+            </form>
+        </>
+    )
+}
 export default Formulario
