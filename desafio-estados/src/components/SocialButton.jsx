599b2a4f57e0156cb96cbdd30024910e7da907f4
Jotape <juanpa1999a@gmail.com>
2023-08-25 15:32:54 -0400

    Se agrego props de Registro a SocialButton

--- desafio-estados/src/components/SocialButton.jsx
+++ desafio-estados/src/components/SocialButton.jsx
@@ -1,9 +1,9 @@
-const SocialButton = () =>{
+const SocialButton = (redA,redB,redC) =>{
     return (
         <div className="social-button-container">
-            <i className="bi bi-facebook"></i>
-            <i className="bi bi-github"></i>
-            <i className="bi bi-linkedin"></i>
+            <i className={"bi bi-"+{redA}}></i>
+            <i className={"bi bi-"+{redB}}></i>
+            <i className={"bi bi-"+{redC}}></i>
         </div>
     )
 }
