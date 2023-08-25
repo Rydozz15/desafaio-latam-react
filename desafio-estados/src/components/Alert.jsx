e7926d871e7b5e764c68a16dc75807e12c672e39
Jotape <juanpa1999a@gmail.com>
2023-08-25 13:03:20 -0400

    Se establece un sistema de error de 0 a 2 para errores que incluyan verificación y cuenta exitosa

--- desafio-estados/src/components/Alert.jsx
+++ desafio-estados/src/components/Alert.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+import Formulario from "./Formulario";
+
+//Pasar de boolean a 0,1,2?¡
 const Alert = () =>{
         //Estado para los errores
-        const [error, setError] = useState(false);
+        const [error, setError] = useState(0);
         //Función antes de enviar el formulario
         const validarDatos = (e) => {
             e.preventDefault();
             //Validación;
             if (nombre === '' || email === '' || contrasena === '' || verif === ''){
-                setError(true);
+                setError(0);
                 return;
+            } else if (contrasena != verif){
+                setError(1)
+                return
+            } else{
+                setError(2);
+                return
             }
-            setError(false);
         };
     return (
-
+        <>
+            {error == 0 ? <div className="alert alert-danger" role="alert">Todos los componentes son obligatorios</div> 
+            : error == 1 ? <div className="alert alert-warning" role="alert">Las contraseñas no coinciden</div> 
+            : error == 2 ? <div className="alert alert-success" role="alert">Tu registro fue exitoso</div>
+            : null}
+        </>
     )
 }
 export default Alert
