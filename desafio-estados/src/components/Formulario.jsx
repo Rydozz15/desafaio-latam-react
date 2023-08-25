94a06221f447a1733823bcd2952bca84fbff6445
Jotape <juanpa1999a@gmail.com>
2023-08-25 12:19:38 -0400

    Se agrgan inputs para email, contrasena y verif

--- desafio-estados/src/components/Formulario.jsx
+++ desafio-estados/src/components/Formulario.jsx
@@ -1,8 +1,8 @@
 const Formulario = () => {
     //Estados del formulario
     const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
     const [email, setEmail] = useState('');
+    const [contrasena, setContrasena] = useState('');
     const [verif, setVerif] = useState('');
 
     return (
@@ -10,6 +10,9 @@ const Formulario = () => {
             <form onSubmit={} className="formulario">
                 <div className="form-group">
                     <input type="text" name="nombre" placeholder="Nombre" className="formControl" onChange={(e) => setNombre(e.target.value)} value={nombre}/>
+                    <input type="email" name="email" placeholder="tumail@ejemplo.com" className="formControl" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <input type="password" name="contrasena" placeholder="Contraseña" className="formControl" onChange={(e) => setContrasena(e.target.value)} value={contrasena}/>
+                    <input type="password" name="verificacion" placeholder="Confirma tu contraseña" className="formControl" onChange={(e) => setVerif(e.target.value)} value={verif}/>
                 </div>
             </form>
         </>
