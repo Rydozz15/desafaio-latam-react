720ff8c711acdf91fc98f5f4ffea4f5b31ba2a89
Jotape <juanpa1999a@gmail.com>
2023-08-25 12:35:05 -0400

    Se cambió a Alert el componente de verificacion

--- desafio-estados/src/components/Alert.jsx
+++ desafio-estados/src/components/Alert.jsx
@@ -1,1 +1,18 @@
+const Alert = () =>{
+        //Estado para los errores
+        const [error, setError] = useState(false);
+        //Función antes de enviar el formulario
+        const validarDatos = (e) => {
+            e.preventDefault();
+            //Validación;
+            if (nombre === '' || email === '' || contrasena === '' || verif === ''){
+                setError(true);
+                return;
+            }
+            setError(false);
+        };
+    return (
+
+    )
+}
 export default Alert
