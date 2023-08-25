9e1c02e321a8107b6f4e9162462d003fb7f7473f
Jotape <juanpa1999a@gmail.com>
2023-08-25 13:05:43 -0400

    Se agregaron headers

--- desafio-estados/src/components/Alert.jsx
+++ desafio-estados/src/components/Alert.jsx
@@ -22,9 +22,9 @@ const Alert = () =>{
         };
     return (
         <>
-            {error == 0 ? <div className="alert alert-danger" role="alert">Todos los componentes son obligatorios</div> 
-            : error == 1 ? <div className="alert alert-warning" role="alert">Las contraseñas no coinciden</div> 
-            : error == 2 ? <div className="alert alert-success" role="alert">Tu registro fue exitoso</div>
+            {error == 0 ? <div className="alert alert-danger" role="alert"><h4 class="alert-heading">Hay campos vacíos</h4>Todos los componentes son obligatorios</div> 
+            : error == 1 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>Las contraseñas no coinciden</div> 
+            : error == 2 ? <div className="alert alert-success" role="alert"><h4 class="alert-heading">Completado</h4>Tu registro fue exitoso</div>
             : null}
         </>
     )
