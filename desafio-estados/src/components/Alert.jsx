5cb4e11d1c7b12d09601850d1086688c9811ee03
Jotape <juanpa1999a@gmail.com>
2023-08-25 16:00:36 -0400

    Se agrega regEx de email tanto a validarDatos como en Alert

--- desafio-estados/src/components/Alert.jsx
+++ desafio-estados/src/components/Alert.jsx
@@ -1,30 +1,11 @@
-import { useState } from "react";
-import Formulario from "./Formulario";
-
 //Pasar de boolean a 0,1,2?¡
-const Alert = () =>{
-        //Estado para los errores
-        const [error, setError] = useState(0);
-        //Función antes de enviar el formulario
-        const validarDatos = (e) => {
-            e.preventDefault();
-            //Validación;
-            if (nombre === '' || email === '' || contrasena === '' || verif === ''){
-                setError(0);
-                return;
-            } else if (contrasena != verif){
-                setError(1)
-                return
-            } else{
-                setError(2);
-                return
-            }
-        };
+const Alert = (error) =>{
     return (
         <>
             {error == 0 ? <div className="alert alert-danger" role="alert"><h4 class="alert-heading">Hay campos vacíos</h4>Todos los componentes son obligatorios</div> 
-            : error == 1 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>Las contraseñas no coinciden</div> 
-            : error == 2 ? <div className="alert alert-success" role="alert"><h4 class="alert-heading">Completado</h4>Tu registro fue exitoso</div>
+            : error == 1 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>Las contraseñas no coinciden</div>
+            : error == 2 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>El email no tiene el formato correcto</div>  
+            : error == 3 ? <div className="alert alert-success" role="alert"><h4 class="alert-heading">Completado</h4>Tu registro fue exitoso</div>
             : null}
         </>
     )
--- desafio-estados/src/components/Formulario.jsx
+++ desafio-estados/src/components/Formulario.jsx
@@ -6,20 +6,31 @@ const Formulario = () => {
     const [email, setEmail] = useState('');
     const [contrasena, setContrasena] = useState('');
     const [verif, setVerif] = useState('');
-
+    
     //Estado para los errores
-    const [error, setError] = useState(false);
+    const [error, setError] = useState(0);
+
+    //Expresion regular del mail
+    let regExEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     //Función antes de enviar el formulario
     const validarDatos = (e) => {
         e.preventDefault();
         //Validación;
         if (nombre === '' || email === '' || contrasena === '' || verif === ''){
-            setError(true);
+            setError(0);
             return;
+        } else if (contrasena != verif){
+            setError(1)
+            return
+        } else if(!regExEmail.test(email)){
+            setError(2);
+            return
+        } else{
+            setError(3)
+            return
         }
-        setError(false);
     };
-
     return (
         <>
             <form onSubmit={validarDatos} className="formulario">
@@ -28,6 +39,7 @@ const Formulario = () => {
                     <input type="email" name="email" placeholder="tumail@ejemplo.com" className="formControl" onChange={(e) => setEmail(e.target.value)} value={email}/>
                     <input type="password" name="contrasena" placeholder="Contraseña" className="formControl" onChange={(e) => setContrasena(e.target.value)} value={contrasena}/>
                     <input type="password" name="verificacion" placeholder="Confirma tu contraseña" className="formControl" onChange={(e) => setVerif(e.target.value)} value={verif}/>
+                    <button type="submit" class="btn btn-secondary btn-lg btn-block" onSubmit={validarDatos}>Registrar</button>
                 </div>
             </form>
         </>
