45e2287ca69ac6961f062393a058d75bc6af3fb3
Jotape <juanpa1999a@gmail.com>
2023-08-26 13:51:58 -0400

    Se importan los iconos instalados y se pasan por props a SB

--- desafio-estados/src/components/Registro.jsx
+++ desafio-estados/src/components/Registro.jsx
@@ -1,3 +1,6 @@
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import SocialButton from "./SocialButton" 
 import Formulario from "./Formulario"
 import Alert from "./Alert"
@@ -6,7 +9,13 @@ const Registro = () =>{
     return (
         <section>
             <h1>Crea una cuenta</h1>
-            <SocialButton redA="facebook" redB="github" redC="linkedin-in"></SocialButton>
+            <div className="container text-center">
+                <div className="row align-items-start">
+                    <SocialButton red={faFacebook}></SocialButton>
+                    <SocialButton red={faLinkedinIn}></SocialButton>
+                    <SocialButton red={faGithub}></SocialButton>
+                </div>
+            </div>
             <p>o usa tu mail para registrarte</p>
             <Formulario></Formulario>
             <Alert></Alert>
