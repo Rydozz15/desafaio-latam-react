345cd88f4ace8690cd942b93749664ac5b33be19
Jotape <juanpa1999a@gmail.com>
2023-08-28 19:25:52 -0400

    Se crea componente de mensajes de error que se pasaran por props hasta Alert

--- desafio-estados/src/App.jsx
+++ desafio-estados/src/App.jsx
@@ -2,9 +2,22 @@ import './App.css'
 import Registro from './components/Registro'
 
 function App() {
+  const mensajesDeAlerta = [
+    {"tipo" : "alert alert-danger",
+    "titulo" : "Hay campos vacíos",
+    "contenido" : "Todos los componentes son obligatorios"},
+    {"tipo" : "alert alert-warning",
+    "titulo" : "Hubo un problema",
+    "contenido" : "Las contraseñas no coinciden"},
+    {"tipo" : "alert alert-warning",
+    "titulo" : "Hubo un problema",
+    "contenido" : "El email no tiene el formato correcto"},
+    {"tipo" : "alert alert-success",
+    "titulo" : "Completado",
+    "contenido" : "Tu registro fue exitoso"}]
   return (
     <>
-      <Registro></Registro>
+      <Registro errorMensaje={mensajesDeAlerta}></Registro>
     </>
   )
 }
