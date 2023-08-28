0458748cd92eac46747760743ebc14eed93e2eb2
Jotape <juanpa1999a@gmail.com>
2023-08-28 19:29:08 -0400

    Props pasan el estado de error y el Array desde Apps

--- desafio-estados/src/components/Alert.jsx
+++ desafio-estados/src/components/Alert.jsx
@@ -1,11 +1,10 @@
-//Pasar de boolean a 0,1,2?¡
-const Alert = (error) =>{
+const Alert = ({ mensaje, errorInterno }) =>{
     return (
         <>
-            {error == 0 ? <div className="alert alert-danger" role="alert"><h4 class="alert-heading">Hay campos vacíos</h4>Todos los componentes son obligatorios</div> 
-            : error == 1 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>Las contraseñas no coinciden</div>
-            : error == 2 ? <div className="alert alert-warning" role="alert"><h4 class="alert-heading">Hubo un problema</h4>El email no tiene el formato correcto</div>  
-            : error == 3 ? <div className="alert alert-success" role="alert"><h4 class="alert-heading">Completado</h4>Tu registro fue exitoso</div>
+            {errorInterno == 0 ? <div className={mensaje[0].tipo} role="alert"><h4 className="alert-heading">{mensaje[0].titulo}</h4>{mensaje[0].contenido}</div> 
+            : errorInterno == 1 ? <div className={mensaje[1].tipo} role="alert"><h4 className="alert-heading">{mensaje[1].titulo}</h4>{mensaje[1].contenido}</div>
+            : errorInterno == 2 ? <div className={mensaje[2].tipo} role="alert"><h4 className="alert-heading">{mensaje[2].titulo}</h4>{mensaje[2].contenido}</div>  
+            : errorInterno == 3 ? <div className={mensaje[3].tipo} role="alert"><h4 className="alert-heading">{mensaje[3].titulo}</h4>{mensaje[3].contenido}</div>
             : null}
         </>
     )
