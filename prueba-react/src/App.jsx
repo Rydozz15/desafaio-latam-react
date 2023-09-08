9729102223c9d12e8bbb8e5a2432d25987dc7b1e
Jotape <juanpa1999a@gmail.com>
2023-09-08 02:34:34 -0300

    Add useStates used for MiApi.jsx and conditional rendering due to a bug in the fetch of Random

--- prueba-react/src/App.jsx
+++ prueba-react/src/App.jsx
@@ -1,11 +1,45 @@
-import './App.css'
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import MiApi from './components/MiApi'
 
 function App() {
-
+const [animeRandom, setAnimeRandom] = useState(0);
+const [animeTop, setAnimeTop] = useState(0);
+console.log(animeTop)
   return (
-    <>
-
-    </>
+    <div>
+      <MiApi setAnimeRandom={setAnimeRandom} setAnimeTop={setAnimeTop}></MiApi>
+      {animeRandom == 0
+      ? '' 
+      : 
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={animeRandom.images.webp['image_url']} />
+        <Card.Body>
+            <Card.Title>{animeRandom.title}</Card.Title>
+            <Card.Text>
+                {animeRandom.synopsis}
+            </Card.Text>
+        </Card.Body>
+      </Card>
+    }
+    {animeTop == 0
+      ? '' 
+      : 
+      animeTop.map(elementoAnime =>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={elementoAnime.images.webp['image_url']} />
+        <Card.Body>
+            <Card.Title>{elementoAnime.title}</Card.Title>
+            <Card.Text>
+                {elementoAnime.synopsis}
+            </Card.Text>
+        </Card.Body>
+      </Card>
+      )
+    }
+    </div>
+    
   )
 }
 
