aa070f5ec12451255ce1e30c03af7a68cc2e8c84
Jotape <juanpa1999a@gmail.com>
2023-09-08 02:33:17 -0300

    Add fetch for Top and Random anime

--- prueba-react/src/components/MiApi.jsx
+++ prueba-react/src/components/MiApi.jsx
@@ -1,6 +1,42 @@
-function MiApi (){
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-    return(
-        
-    )
+function MiApi({setAnimeRandom,setAnimeTop}) {
+
+//1. Guardar los datos
+// GET with fetch API
+useEffect(() => {
+    fetchAnimeRandom();
+    fetchAnimeTop();
+}, []);
+
+const fetchAnimeRandom = async () => {
+    const apiUrl = 'https://api.jikan.moe/v4/random/anime';
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setAnimeRandom(response.data.data);
+      })
+      .catch((err) => {
+        console.log("Error al obtener datos de la API:", err);
+      });
+  };
+
+const fetchAnimeTop= async () => {
+const apiUrl = 'https://api.jikan.moe/v4/top/anime';
+axios
+    .get(apiUrl)
+    .then((response) => {
+    setAnimeTop(response.data.data);
+    })
+    .catch((err) => {
+    console.log("Error al obtener datos de la API:", err);
+    });
+};
+return (
+    <>
+    </>
+)
 }
+
+export default MiApi
