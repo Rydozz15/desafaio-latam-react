74c31db9f495628e8707697632ad942f219c2416
Jotape <juanpa1999a@gmail.com>
2023-09-10 22:42:57 -0300

    Se agrega estilo con Bootstrap

--- prueba-react/src/components/Buscador.jsx
+++ prueba-react/src/components/Buscador.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { useState,useEffect } from "react";
-import { Form } from "react-bootstrap";
-import {Button} from "react-bootstrap";
-import { Dropdown } from "react-bootstrap";
+import { useState} from "react";
+import { Form, Button, Dropdown, Container, Row } from "react-bootstrap";
 
 const Buscador = ({ setAnimeTop,setFilterState}) => {  
   const [animeNameAPI,setAnimeNameAPI] = useState('')
@@ -19,30 +17,35 @@ const Buscador = ({ setAnimeTop,setFilterState}) => {
   
   return (
     <>
-    <Form onSubmit={takeSearchTerm}>
-      <Form.Group className="mb-4">
-        <Form.Control 
-          type="text"
-          name="browser"
-          id="browser"
-          placeholder="Busca tu anime en el top" 
-          onChange={(e) => setAnimeNameAPI(e.target.value)}/>
-          <Button variant="primary" type="submit">Buscar</Button>
-      </Form.Group>
-    </Form>
+    <Container className="d-flex justify-content-center m-3">
+      <Row>
+        <Form className='col-7' onSubmit={takeSearchTerm}>
+          <Form.Group className="mb-4 d-flex justify-content-around">
+            <Form.Control 
+              className="px-5 mx-3"
+              type="text"
+              name="browser"
+              id="browser"
+              placeholder="Busca tu anime en el top" 
+              onChange={(e) => setAnimeNameAPI(e.target.value)}/>
+              <Button variant="primary" type="submit">Buscar</Button>
+          </Form.Group>
+        </Form>
 
-    <Dropdown>
-      <Dropdown.Toggle variant="success">
-        Filtro de orden
-      </Dropdown.Toggle>
+        <Dropdown className='col-4'>
+          <Dropdown.Toggle variant="success">
+            Filtro de orden
+          </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Item onClick={() => setFilterState('Newest')}>Más nuevos</Dropdown.Item>
-        <Dropdown.Item onClick={() => setFilterState('Oldest')}>Más antiguos</Dropdown.Item>
-        <Dropdown.Item onClick={() => setFilterState('Best')}>Mejor evaluados</Dropdown.Item>
-        <Dropdown.Item onClick={() => setFilterState('Worst')}>Peor evaluados</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => setFilterState('Newest')}>Más nuevos</Dropdown.Item>
+            <Dropdown.Item onClick={() => setFilterState('Oldest')}>Más antiguos</Dropdown.Item>
+            <Dropdown.Item onClick={() => setFilterState('Best')}>Mejor evaluados</Dropdown.Item>
+            <Dropdown.Item onClick={() => setFilterState('Worst')}>Peor evaluados</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Row>
+    </Container>
     </>
   );
 };
