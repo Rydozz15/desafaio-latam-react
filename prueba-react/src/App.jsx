b62e4c7add2588a57c2a71121668227abc658940
Jotape <juanpa1999a@gmail.com>
2023-09-10 22:33:14 -0300

    Se integra orden con contenedores y ordenamiento

--- prueba-react/src/App.jsx
+++ prueba-react/src/App.jsx
@@ -1,46 +1,78 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import MiApi from './components/MiApi'
+import Buscador from './components/Buscador';
+import { Container,Row } from 'react-bootstrap';
 
 function App() {
-const [animeRandom, setAnimeRandom] = useState(0);
-const [animeTop, setAnimeTop] = useState(0);
-console.log(animeTop)
+const [animeTop, setAnimeTop] = useState([]);
+const[animeFiltered,setAnimeFiltered] = useState(animeTop)
+const [filterState,setFilterState] = useState('')
+console.log(filterState)
+useEffect(() => {
+  animeFilter(filterState);
+},[filterState,animeTop]);
+
+const animeFilter = (filterState) => {
+  console.log(filterState)
+  console.log(animeFiltered)
+  if(filterState == 'Newest'){
+    animeFiltered.sort((a,b)=>{
+      return new Date(a.aired.from) - new Date(b.aired.from)
+        })
+  }
+  switch (filterState) {
+    case 'Newest':
+      animeFiltered.sort((a,b)=>{
+      return new Date(a.aired.from) - new Date(b.aired.from)
+        }) 
+      break;
+    case 'Oldest':
+      animeFiltered.sort((a,b)=>{
+      return new Date(b.aired.from) - new Date(a.aired.from)
+        })
+      break;
+    case 'Best':
+      animeFiltered.sort((a,b)=>{
+      return a.score - b.score
+        })
+      break;
+    case 'Worst':
+      animeFiltered.sort((a,b)=>{
+      return b.score - a.score
+        })
+      break;
+    default:
+      return ''
+  }
+};
+
   return (
-    <div>
-      <MiApi setAnimeRandom={setAnimeRandom} setAnimeTop={setAnimeTop}></MiApi>
-      {animeRandom == 0
-      ? '' 
-      : 
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={animeRandom.images.webp['image_url']} />
-        <Card.Body>
-            <Card.Title>{animeRandom.title}</Card.Title>
-            <Card.Text>
-                {animeRandom.synopsis}
-            </Card.Text>
-        </Card.Body>
-      </Card>
-    }
-    {animeTop == 0
-      ? '' 
-      : 
-      animeTop.map(elementoAnime =>
-        //Hay que cambiar esto para que quede ordenado
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={elementoAnime.images.webp['image_url']} />
-        <Card.Body>
-            <Card.Title>{elementoAnime.title}</Card.Title>
-            <Card.Text>
-                {elementoAnime.synopsis}
-            </Card.Text>
-            <Button variant='success' href={elementoAnime.url} target="_blank">More info.</Button>
-        </Card.Body>
-      </Card>
-      )
-    }
-    </div>
+    <>
+      <Buscador setAnimeTop={setAnimeTop} setFilterState={setFilterState} ></Buscador>
+      <MiApi setAnimeFiltered={setAnimeFiltered} animeTop={animeTop}></MiApi>
+      <Container>
+        <Row>
+          {animeFiltered == []
+          ? '' 
+          : 
+          animeFiltered.map(elementoAnime =>
+            <Card key={elementoAnime.title} style={{ width: '30%' }} className='col-4'>
+            <Card.Img variant="top" src={elementoAnime.images.webp['image_url']} />
+            <Card.Body>
+                <Card.Title>{elementoAnime.title}</Card.Title>
+                <Card.Text>
+                    {elementoAnime.synopsis}
+                </Card.Text>
+                <Button variant='success' href={elementoAnime.url} target="_blank">More info.</Button>
+            </Card.Body>
+          </Card>
+          )
+        }
+        </Row>
+      </Container>
+    </>
     
   )
 }
