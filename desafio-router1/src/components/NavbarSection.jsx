61e60271d2175cccba1e4216edf9bbad2d826a03
Jotape <juanpa1999a@gmail.com>
2023-09-29 18:35:24 -0300

    Add Navbar with bootstrap and Links from Router

--- desafio-router1/src/components/NavbarSection.jsx
+++ desafio-router1/src/components/NavbarSection.jsx
@@ -1,9 +1,24 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Navbar } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 function NavbarSection() {
     return(
         <>
+            <Navbar bg="danger" data-bs-theme="dark">
+                <Container>
+                    <Link
+                    to="/"
+                    className="text-white ms-3 text-decoration-none">
+                        Home 🏡
+                    </Link>
+                    <Link
+                    to="/contact"
+                    className="text-white ms-3 text-decoration-none">
+                        Contacto 📧
+                    </Link>
+                    <Navbar.Brand>Happy Cake 🎂</Navbar.Brand>
+                </Container>
+            </Navbar>
         </>
     )
 }
