e1a8c3ffe31d7bc50d997709d71a562c06e61e27
Jotape <juanpa1999a@gmail.com>
2023-09-29 19:27:28 -0300

    Add Routes to redirect pages

--- desafio-router1/src/App.jsx
+++ desafio-router1/src/App.jsx
@@ -1,4 +1,9 @@
+import { Route, Routes } from "react-router-dom"
+
 import NavbarSection from "./components/NavbarSection"
+import HomePage from "./views/HomePage"
+import ContactPage from "./views/ContactPage"
+import NotFound from "./views/NotFound"
 
 function App() {
   
@@ -6,6 +11,20 @@ function App() {
   return (
     <>
       <NavbarSection />
+      <Routes>
+        <Route 
+        path="/"
+        element={<HomePage />}
+        />
+        <Route 
+        path="/contact"
+        element={<ContactPage />}
+        />
+        <Route 
+        path="*"
+        element={<NotFound />}
+        />
+      </Routes>
     </>
   )
 }
