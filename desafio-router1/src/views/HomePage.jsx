b13aab7a5a12996bf97610365327fa867813ff46
Jotape <juanpa1999a@gmail.com>
2023-09-29 19:05:30 -0300

    Add Home content

---
+++ desafio-router1/src/views/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h1>Bienvenido a <span className="font-weight-bold">Happy Cake</span></h1>
+            <p>El lugar de los pasteles felices</p>
+            <h1>🍰</h1>
+        </div>
+    )
+}
+export default HomePage
