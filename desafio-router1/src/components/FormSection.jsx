b44e22eb168828d8aacedae6d857c3efa1cce6cc
Jotape <juanpa1999a@gmail.com>
2023-09-29 18:43:27 -0300

    Add template Form from Bootstrap Docs

---
+++ desafio-router1/src/components/FormSection.jsx
@@ -0,0 +1,23 @@
+import { Button, Form } from "react-bootstrap";
+
+function FormSection() {
+    return(
+        <>
+            <Form>
+            <Form.Group className="mb-3">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form>
+        </>
+    )
+}
+
+export default FormSection
