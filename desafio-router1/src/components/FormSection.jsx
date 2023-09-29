a8d9d80abbf92a8ded3dc32baad426a582740c55
Jotape <juanpa1999a@gmail.com>
2023-09-29 18:57:24 -0300

    Set Navbar first Draft with Flexbox

--- desafio-router1/src/components/FormSection.jsx
+++ desafio-router1/src/components/FormSection.jsx
@@ -3,18 +3,20 @@ import { Button, Form } from "react-bootstrap";
 function FormSection() {
     return(
         <>
-            <Form>
-            <Form.Group className="mb-3">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+            <Form className="d-flex flex-column justify-content-center">
+            <Form.Group className="d-flex flex-column align-items-center mb-3">
+                <Form.Label>Correo</Form.Label>
+                <Form.Control type="email" placeholder="nombre@ejemplo.com" />
             </Form.Group>
-            <Form.Group className="mb-3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="d-flex flex-column align-items-center mb-3">
+                <Form.Label>Descripción</Form.Label>
+                <Form.Control type="text" placeholder="Escribe lo que quieras :)" />
             </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
+            <Form.Group className="d-flex flex-column align-items-center">
+                <Button variant="danger" type="submit">
+                    Enviar
+                </Button>
+            </Form.Group>
             </Form>
         </>
     )
