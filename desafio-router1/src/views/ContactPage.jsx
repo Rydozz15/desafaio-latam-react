5e502e774948ee9b711eb1331cfceea581bb8ed9
Jotape <juanpa1999a@gmail.com>
2023-09-29 19:10:52 -0300

    Add Contact Page content and Form component

---
+++ desafio-router1/src/views/ContactPage.jsx
@@ -0,0 +1,11 @@
+import FormSection from "../components/FormSection"
+
+function ContactPage() {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h1>Cuéntanos, ¿en qué te podemos ayudar?</h1>
+            <FormSection />
+        </div>
+    )
+}
+export default ContactPage
