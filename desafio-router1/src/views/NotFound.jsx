44a54278eeaf4cbcd4bd7d03379b20f1d26624e0
Jotape <juanpa1999a@gmail.com>
2023-09-29 19:26:06 -0300

    Add content and export element of NotFound

---
+++ desafio-router1/src/views/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <h1>Oops!</h1>
+            <h2>No se ha podido encontrar la página</h2>
+            <p>Nanai, aquí tienes una flor 🌻 Intenta apretar una opción de la barra de navegación para salir :D</p>
+        </div>
+    )
+}
+export default NotFound
